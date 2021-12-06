fedf4cc717cf5c09fef8db2db2617ffb028a8a81
Damian Romero <damian.alexis.romero@gmail.com>
2021-12-05 23:05:43 -0300

    inicio

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+/* const productos = JSON.parse(importScripts(./js/productos.json))
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+} */
+
+const articulo = document.querySelector(".articles__cards");
+console.log(articulo);
+
+
+
+
+
+
+{/* <div class="col-sm-12 col-md-6 col-lg-4 cards__item">
+<div class="cards__imag cards__imag--new card">
+  <img src="./multimedia/acuarela_filgo.jpg" class="card-img-top" alt="Acuarela filgo" title="Acuarela escolar filgo de 12 colores con un pincel de regalo">
+</div>
+<div class="cards__body card-body">
+  <a class="cards__paragraph text-reset" href="#">
+    <p class="cards__details">Acuarela Paleta Filgo X 12 Colores + Pincel. Ideal para las manualidades escolares. Fácil lavado</p>
+  </a>
+</div>
+</div> */}
